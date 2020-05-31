09cd35ce1f87b945cbd3b1ad4beb1529240ae457
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-05-31 04:06:03 +0300

    experience and contact controller are created.

---
+++ src/controllers/ContactController.js
@@ -0,0 +1,150 @@
+const Joi = require('@hapi/joi');
+const models = require('../models');
+const Contact = models.socialMedias;
+const ContactForm = models.contactForms;
+
+exports.socialMedias = function(req, res) {
+  const data = req.params
+  Contact.findAll({
+    order: [
+      ['sequence', 'ASC']
+    ]
+  }).then(data => {
+    res.send(data);
+  });
+};
+
+exports.createSocialMedia = function(req, res) {
+
+  const data = req.body;
+
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    link: Joi.string().required(),
+    color: Joi.string().allow(null),
+    sequence: Joi.number().required(),
+  });
+
+  const { error }= schema.validate(data);
+
+  if (error) {
+    res.status(422).json({
+        status: 'error',
+        message: error.details,
+    });
+  } else {
+    Contact.create({ 
+      name: data.name,
+      link: data.link,
+      color: data.color,
+      sequence: data.sequence,
+    }).then(data => {
+      res.status(200).json(data);
+    });
+  }
+};
+
+exports.updateSocialMedia = function(req, res) {
+
+  const data = req.body;
+
+  const schema = Joi.object({
+    id: Joi.number().required(),
+    name: Joi.string().required(),
+    link: Joi.string().required(),
+    color: Joi.string().allow(null),
+    sequence: Joi.number().required(),
+  });
+
+  const { error }= schema.validate(data);
+
+  if (error) {
+    res.status(422).json({
+        status: 'error',
+        message: error,
+    });
+  } else {
+    Contact.update({ 
+      name: data.name,
+      link: data.link,
+      color: data.color,
+      sequence: data.sequence,
+      updatedAt: Date.now()
+    },{
+      where: {
+        id: data.id
+      }
+    }).then((result) => {
+      res.status(200).json(result);
+    }).catch(error => {
+      res.status(400).json(error);
+    });
+  }
+};
+
+exports.deleteSocialMedia = function(req, res) {
+  const data = req.params 
+  Contact.destroy({
+    where: {
+      id: data.id
+    }
+  }).then((result) => {
+    res.status(200).json(result);
+  }).catch(error => {
+    res.status(400).json(error);
+  });
+}
+
+exports.contactForms = function(req, res) {
+  const data = req.params
+  ContactForm.findAll({
+    order: [
+      ['sequence', 'ASC']
+    ]
+  }).then(data => {
+    res.send(data);
+  });
+};
+
+exports.createContactForm = function(req, res) {
+
+  const data = req.body;
+
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    surname: Joi.string().required(),
+    email: Joi.string().email(),
+    description: Joi.string().required()
+  });
+
+  const { error }= schema.validate(data);
+
+  if (error) {
+    res.status(422).json({
+        status: 'error',
+        message: error.details,
+    });
+  } else {
+    ContactForm.create({ 
+      name: data.name,
+      surname: data.surname,
+      email: data.email,
+      description: data.description,
+    }).then(data => {
+      res.status(200).json(data);
+    });
+  }
+};
+
+exports.deleteContactForm = function(req, res) {
+  const data = req.params 
+  ContactForm.destroy({
+    where: {
+      id: data.id
+    }
+  }).then((result) => {
+    res.status(200).json(result);
+  }).catch(error => {
+    res.status(400).json(error);
+  });
+}
---
+++ src/controllers/ExperienceController.js
@@ -0,0 +1,106 @@
+const Joi = require('@hapi/joi');
+const models = require('../models');
+const Experiences = models.experiences;
+
+exports.experiences = function(req, res) {
+  const data = req.params
+  Experiences.findAll({
+    where: {
+      lang: data.lang
+    },
+    order: [
+      ['startingDate', 'DESC']
+    ]
+  }).then(data => {
+    res.send(data);
+  });
+};
+
+exports.createExperience = function(req, res) {
+
+  const data = req.body;
+
+  const schema = Joi.object({
+    title: Joi.string().required(),
+    company: Joi.string().required(),
+    description: Joi.string().allow(null),
+    startingDate: Joi.date().required(),
+    terminationDate: Joi.date().allow(null),
+    lang: Joi.string().required()
+  });
+
+  const { error }= schema.validate(data);
+
+  if (error) {
+    res.status(422).json({
+        status: 'error',
+        message: error.details,
+    });
+  } else {
+    Experiences.create({ 
+      title: data.title,
+      company: data.company,
+      description: data.description,
+      startingDate: data.startingDate,
+      terminationDate: data.terminationDate,
+      lang: data.lang,
+    }).then(data => {
+      res.status(200).json(data);
+    });
+  }
+};
+
+exports.updateExperience = function(req, res) {
+
+  const data = req.body;
+
+  const schema = Joi.object({
+    id: Joi.number().required(),
+    title: Joi.string().required(),
+    company: Joi.string().required(),
+    description: Joi.string().allow(null),
+    startingDate: Joi.date().required(),
+    terminationDate: Joi.date().allow(null),
+    lang: Joi.string().required()
+  });
+
+  const { error }= schema.validate(data);
+
+  if (error) {
+    res.status(422).json({
+        status: 'error',
+        message: error,
+    });
+  } else {
+    Experiences.update({ 
+      title: data.title,
+      company: data.company,
+      description: data.description,
+      startingDate: data.startingDate,
+      terminationDate: data.terminationDate,
+      lang: data.lang,
+      updatedAt: Date.now()
+    },{
+      where: {
+        id: data.id
+      }
+    }).then((result) => {
+      res.status(200).json(result);
+    }).catch(error => {
+      res.status(400).json(error);
+    });
+  }
+};
+
+exports.deleteExperience = function(req, res) {
+  const data = req.params 
+  Experiences.destroy({
+    where: {
+      id: data.id
+    }
+  }).then((result) => {
+    res.status(200).json(result);
+  }).catch(error => {
+    res.status(400).json(error);
+  });
+}
