6d194045c59a6637addc5de161c573bf0e7b974a
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-06-01 20:46:17 +0300

    build integrated

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config()
 
+const prosess = {
+  db: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER,
+  pass: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: process.env.PORT,
+  url: process.env.DATABASE_URL,
+}
+console.log({})
 const sequelize = new Sequelize({
   database: process.env.DATABASE_NAME,
   username: process.env.DATABASE_USER,
